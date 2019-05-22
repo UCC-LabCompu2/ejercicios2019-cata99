275f1bfcbe2aaddbb841317b8f6b98de192a9d13
cata99 <catupaschini@hotmail.com>
2019-05-22 09:49:40 -0300

    empezo a resolver ejercicios java

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,3 +9,23 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function saludar(){
+    alert("hola mmundo!");
+}
+
+function convertir(id_elem, valor_elem){
+    if(isNaN(valor_elem)){
+        alert("El valor de "+id_elem+" debe ser numerico.");
+        document.getElementById(id_elem).value= "";
+        }
+    if(!isNaN(valor_elem)) {
+        if (id_elem == 'metro') {
+            document.getElementById("pulgada").value = valor_elem * 39.3701;
+            document.getElementById("pie").value = valor_elem * 3.28084;
+            document.getElementById("yarda").value = valor_elem * 1.09361;
+        }
+        if(id_elem=='pulgada'){
+            document.getElementById("metro").value=valor_elem*
+        }
+    }
+}
