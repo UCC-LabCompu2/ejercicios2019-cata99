3e5968a7aa8c197320040f0eddc74958e2798e72
cata99 <catupaschini@hotmail.com>
2019-06-05 09:51:30 -0300

    ejercicios java

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -75,3 +75,56 @@ function mo(id_elem) {
         document.getElementById("unDiv").style.display='block';
     }
 }
+
+function dibujarCircCuadr(){
+    var canvas =document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#ff273c";
+    ctx.beginPath();
+    ctx.rect(0,0,30,40);
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.fillStyle = "#af3679";
+    ctx.beginPath();
+    ctx.arc(canvas.width/2,canvas.width/2,18,0,2*Math.PI);
+    ctx.fill();
+    ctx.closePath()
+
+}
+
+function dibujarCuadriculado (){
+    var canvas =document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle= "#0000";
+    ctx.beginPath();
+    for (var i=10;i<canvas.width;i+=10) {
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, canvas.height);
+    }
+
+    for (var i=10;i<canvas.width;i+=10) {
+        ctx.moveTo(0, i);
+        ctx.lineTo(canvas.width, i);
+    }
+    ctx.stroke();
+    ctx.closePath();
+
+}
+
+function dibujarImg() {
+    var canvas= document.getElementById("myCanvas");
+    var ctx= canvas.getContext("2d");
+
+    var img= new Image();
+    img.src= "images/auto.png";
+
+    img.onload=function (){
+        ctx.drawImage(img,50,50);
+    }
+
+
+}
